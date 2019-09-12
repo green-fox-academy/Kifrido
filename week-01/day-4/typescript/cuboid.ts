02ece24bb4295c39e476c0093d215ffdf7261960
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-12 17:35:43 +0200

    added cuboid.ts so far not done

---
+++ week-01/day-4/typescript/cuboid.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+const a: number = 10;
+const b: number = 10;
+const c: number = 10;
+console.log(surfaceArea = 2 * (a * b + b * c +c * a));
+console.log(volume = a * b * c);
+
+/*
+let pi: number = 3.141592;
+console.log(pi);
+
+*/
