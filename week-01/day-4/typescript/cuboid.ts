74d4a544de62f8d457cd4d5cc59fe4f136b2bec8
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-13 08:58:00 +0200

    corrected cuboid.ts

--- week-01/day-4/typescript/cuboid.ts
+++ week-01/day-4/typescript/cuboid.ts
@@ -9,8 +9,8 @@
 const a: number = 10;
 const b: number = 10;
 const c: number = 10;
-console.log(surfaceArea = 2 * (a * b + b * c +c * a));
-console.log(volume = a * b * c);
+console.log('Surface Area = ' + (2 * (a * b + b * c + c * a)));
+console.log('Volume = ' + a * b * c);
 
 /*
 let pi: number = 3.141592;
