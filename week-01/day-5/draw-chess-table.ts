595247cf129ae1f681e65a3810bb1bf37a752a70
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-15 18:26:54 +0200

    added chess table

---
+++ week-01/day-5/draw-chess-table.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+// works with even numbers
+
+let lineCount: number = 8;
+let row1: string = "";
+let row2: string = "";
+
+for(let i=0; i< lineCount / 2; i++){
+    row1 += "% ";
+    row2 += " %";
+}
+
+for(let j=0; j< lineCount / 2; j++){
+    console.log(row1);
+    console.log(row2);
+}
+
