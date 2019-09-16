417421587e6ecb53caa14cfc78a29c14d5f2499c
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-16 17:29:44 +0200

    added factorio

---
+++ week-02/day-1/factorio.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// -  Create a function called `factorio`
+//    that returns it's input's factorial
+
+
+let num: number = 5;
+
+function factorio(num) { 
+    let result = 1;
+    for (let i= 1; i<= num; i++){
+        result *= i;
+    }
+    return result;
+  }
+
+  console.log(factorio(num));
+  
