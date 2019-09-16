2ddf874d27a694683ae75cdff627e9e61185111c
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-16 16:39:44 +0200

    took out unneceserry comment

--- week-02/day-1/doubling.ts
+++ week-02/day-1/doubling.ts
@@ -4,9 +4,6 @@
 // -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
 // -  Log the result of `doubling(baseNum)` to the console
 
-/*function doubling (){
-
-} */
 
 let baseNum: number = doubling(123);
 
