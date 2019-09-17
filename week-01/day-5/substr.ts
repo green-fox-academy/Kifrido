d43253a63bf4eedfa4898ebbe7f37ee2e31338c6
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-17 10:25:17 +0200

    added substr.ts

---
+++ week-01/day-5/substr.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+//  Create a function that takes two strings as a parameter
+//  Returns the starting index where the second one is starting in the first one
+//  Returns `-1` if the second string is not in the first one
+
+/*unction substr(str: string, keyword: string) : number {
+  return -1;
+ 
+} */
+
+
+let word: string = "honeymoon";
+let word2: string = "moon";
+console.log(word.indexOf("moon"));
+
+
+
+
+
+//  Example
+
+// should print: `17`
+// console.log(substr("this is what I'm searching in", "searching"));
+
+// should print: `-1`
+// console.log(substr("this is what I'm searching in", "not"));
