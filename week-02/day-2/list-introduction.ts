89eff4510c051e437377c8122121bfbaeda6452c
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-17 16:27:40 +0200

    added list introduction

---
+++ week-02/day-2/list-introduction.ts
@@ -0,0 +1,42 @@
+let listOfNames: string[] = [];
+    console.log(listOfNames.length);
+
+    listOfNames.push ("William");
+
+console.log(listOfNames.length > 0);
+
+
+listOfNames.push ("John", "Amanda");
+
+console.log(listOfNames.length);
+
+console.log(listOfNames[2]);
+
+console.log("------------------");
+
+listOfNames.forEach(element => {
+    console.log(element);
+   
+ });
+
+ console.log("------------------");
+
+ listOfNames.forEach(function(e, i) {
+     console.log(i  + 1 + '.' + e);
+    });
+
+    console.log("------------------");
+
+    let removeItem = listOfNames.splice (1, 1);
+
+    console.log(listOfNames);
+ 
+    console.log("------------------");
+
+    for (let i = listOfNames.length - 1; i >=0 ; i--) {
+        console.log(listOfNames[i]);
+    }
+
+    console.log("------------------");
+
+    listOfNames = [];
