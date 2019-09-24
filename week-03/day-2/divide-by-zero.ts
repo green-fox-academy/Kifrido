541bf345c78138b6e08e3275781402f73b89138c
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-24 13:40:51 +0200

    added divided by zero

---
+++ week-03/day-2/divide-by-zero.ts
@@ -0,0 +1,21 @@
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+
+function dividedByZero(x: number) {
+    
+  try {
+      if(x == 0) {
+          throw "error";
+      } else {(console.log (10 / x));
+    }
+}
+    catch(error) {
+        console.log('fail');
+    } 
+  } 
+
+  dividedByZero(10);
+  dividedByZero(0);
