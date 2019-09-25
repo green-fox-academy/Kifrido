70c18b6b59717d7a4f7749fdfc91d70a46c4a325
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-25 07:38:28 +0200

    added homework

---
+++ week-03/day-3/homework.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+//Create 5 trees
+//Store the data of them in variables in your program
+//for every tree the program should store its'
+//type
+//leaf color
+//age
+//sex
+//you can use just variables, or lists and/or maps
+
+class appleTree {
+    type: 'apple';
+    leafColor: 'green';
+    age: 100;
+    sex: 'male';
+}
+
+class PalmTree {
+    type: 'palm';
+    leafColor: 'green';
+    age: 20;
+    sex: 'male';
+}
+
+class nutTree {
+    type: 'nut';
+    leafColor: 'brown';
+    age: 55;
+    sex: 'female';
+}
+
+class oakTree {
+    type: 'oak';
+    leafColor: 'green';
+    age: 120;
+    sex: 'male';
+}
+
+class blackCherryTree {
+    type: 'blackcherry';
+    leafColor: 'green';
+    age: 80;
+    sex: 'female';
+}
