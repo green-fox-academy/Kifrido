960b98961e15c029902d6de00d5502e709a354a2
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-26 09:27:28 +0200

    added pokemon

---
+++ week-03/day-3/pokemon/main.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+import { Pokemon } from './Pokemon'
+
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
+
+// Which pokemon should Ash use?
+
+//console.log('I choose you, ');
+
+function initializePokemon(): Pokemon[] {
+    return [
+        new Pokemon('Balbasaur', 'leaf', 'water'),
+        new Pokemon('Pikatchu', 'electric', 'water'),
+        new Pokemon('Charizard', 'fire', 'leaf'),
+        new Pokemon('Balbasaur', 'water', 'fire'),
+        new Pokemon('Kingler', 'water', 'fire')
+    ];
+}
+
+function findPokemon (array) : string {
+    for (let i: number = 0; i < array.length; i++){
+        if (array[i].effectiveAgainst == wildPokemon.type){
+        return array[i].name;
+        }
+    }
+}
+
+console.log('I choose you, ' + findPokemon(pokemonOfAsh));
+
+
