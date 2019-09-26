6f0ec4db85f2cfbf390473a5d65599ba43e25a3e
Krisztina Nagy <krisztinahont@hotmail.com>
2019-09-26 14:46:11 +0200

    added diceset

---
+++ week-03/day-4/diceset/DiceSet.ts
@@ -0,0 +1,67 @@
+'use strict';
+
+class DiceSet {
+    dice: number[];
+    readonly numOfDices: number = 6;
+  
+    roll(): number[] {
+      this.dice = [];
+      for (let i = 0; i < this.numOfDices; i++) {
+        this.dice.push(Math.floor(Math.random() * 6 + 1));
+      }
+      return this.dice;
+    }
+  
+    reroll(index?: number): void {
+      if (index === undefined) {
+        for (let i = 0; i < this.numOfDices; i++) {
+          this.dice[i] = Math.floor(Math.random() * 6 + 1);
+        }
+      } else {
+        this.dice[index] = Math.floor(Math.random() * 6 + 1);
+      }
+    }
+  
+    getCurrent(index?: number): number[] | number {
+      if (index === undefined) {
+        for (let i = 0; i < this.numOfDices; i++) {
+          return this.dice;
+        }
+      } else {
+        return this.dice[index];
+      }
+    }
+}
+  
+  // You have a `DiceSet` class which has a list for 6 dice
+  // You can roll all of them with roll()
+  // Check the current rolled numbers with getCurrent()
+  // You can reroll with reroll()
+  // Your task is to roll the dice until all of the dice are 6
+    
+let diceSet = new DiceSet();
+
+console.log(diceSet.roll());
+let counter: number = 0;
+
+function isAllDiceSix(diceSet: DiceSet) : boolean{
+  for(let i = 0; i < diceSet.dice.length; i++){
+    if (diceSet.dice[i] !==6){
+      return false;
+    }
+  }
+
+  return true;
+}
+
+  while (!isAllDiceSix(diceSet)){
+    for(let j=0; j < diceSet.dice.length; j++){
+      if (diceSet.dice[j] !==6){
+      diceSet.reroll(j);
+      }
+    counter++;
+    }
+  }
+
+console.log(diceSet.dice);
+console.log(counter);
