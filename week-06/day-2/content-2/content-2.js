2c130be8e184ef3e80320cc28b7371a4e2091d08
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-22 15:03:37 +0200

    added first half of the exercise

---
+++ week-06/day-2/content-2/content-2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//1) replace the list items' content with items from this list:
+//     ['apple', 'banana', 'cat', 'dog']
+
+
+let newList = ['apple', 'banana', 'cat', 'dog'];
+
+let placeHolder = document.querySelectorAll("li");
+
+  for(let i = 0; i < newList.length; i++){
+      //console.log(placeHolder[i]);
+      placeHolder[i].innerText = newList[i];
+  }
+
+  //    2) change the <ul> element's background color to 'limegreen'
+//     - use css class to change the color instead of the style property
+
+
