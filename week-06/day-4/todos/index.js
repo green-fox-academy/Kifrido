6bd37f2bda668e547ec33b0e60089b540f1381a1
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-24 15:21:39 +0200

    put to do list outside the render

--- week-06/day-4/todos/index.js
+++ week-06/day-4/todos/index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const todos = [
+    'get up',
+    'survive',
+    'go back to bed',
+  ];
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -13,7 +19,7 @@ app.get('/', (req, res) => {
     res.render('home', {
         title: 'Todos',
         h1: 'Things to do today:',
-        todos : ['get up', 'survive', 'go back to bed']
+        todos : todos
       });
     });
 
