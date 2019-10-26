c74856620aa757dd9e7ec4dbc0c7e52c7007da79
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-26 12:05:17 +0200

    added greenfox logo and made it mad

---
+++ week-06/day-2/greenfox-logo/greenfox-logo.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//Select left and right eyes
+ //They have #left-eye and #right-eye
+//turn their color (fill) to tomato
+
+let makeMadLeft = document.querySelector('#left-eye');
+let makeMadRight = document.querySelector('#right-eye');
+makeMadLeft.style.fill = 'tomato';
+makeMadRight.style.fill = 'tomato';
