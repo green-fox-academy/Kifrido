5e974ee500cc694d63b4c5b315733d3750389c1d
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-29 11:22:00 +0100

    added more solutions to simple timeout

--- week-07/day-2/simple_timeout/simple-timeout.js
+++ week-07/day-2/simple_timeout/simple-timeout.js
@@ -2,21 +2,43 @@
 
 // Write a program that prints apple after 3 seconds.
 
+
+//simplest solution
+
 /*let showApple = function(){
     console.log("apple");
 
     setTimeout(showApple, 3000);
 }*/
 
+
+//2. solution
+
 /*setTimeout(function(){
     console.log("apple");
 }, 3000);*/
 
-let showApple = function(){
+
+//writing in the html
+
+/*let showApple = function(){
     let container = document.querySelector('.apple');
     let word = document.createElement('p');
     word.innerText = "apple";
     container.appendChild(word);
 }
-setTimeout(showApple, 3000);
+
+setTimeout(showApple, 3000);*/
+
+//using 2 functions
+
+showApple(function () {
+    console.log('apple');
+});
+
+function showApple(myFruit) {
+    setTimeout(function () {
+        myFruit();
+    }, 3000);
+}
 
