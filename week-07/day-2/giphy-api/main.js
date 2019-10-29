c8b0b1e0f9a93bca5e881d8ca4dddc9d3884152d
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-29 15:53:01 +0100

    added giphy-api

---
+++ week-07/day-2/giphy-api/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//let giphyContainer = document.getElementById("giphy");
+let body = document.getElementsByTagName("body")[0];
+
+    let ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=oJCl7QGYVPWPgWY6VFgkpcH8WzJwaFkj&q=lion&limit=16&offset=0&rating=G&lang=en', true);
+   
+    ourRequest.onload = function(){
+        let ourData = JSON.parse(ourRequest.responseText);
+        for(let i = 0; i < 16; i++){
+            let newImg = document.createElement("img");
+            newImg.setAttribute("src", ourData.data[i].images.fixed_height_small_still.url);
+            newImg.addEventListener("click", function(){
+                newImg.setAttribute("src", ourData.data[i].images.downsized_large.url);;
+            })
+            body.appendChild(newImg);
+        }
+};
+ourRequest.send();
+
