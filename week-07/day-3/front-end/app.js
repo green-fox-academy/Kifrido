e65e752b6169604e146c5c9c9da6f28bc2b626d0
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-30 13:33:19 +0100

    added greeter

--- week-07/day-3/front-end/app.js
+++ week-07/day-3/front-end/app.js
@@ -12,19 +12,34 @@ app.get('/', (req, res) => {
 
 app.get('/doubling', (req, res) => {
     let doubling = {};
-    console.log(req.query.input);
+    res.status(200);
     res.setHeader("Content-type", "application/JSON");
     if (req.query.input !== undefined) {
-        res.status(200);
         doubling['received'] = req.query.input;
         doubling['result'] = req.query.input * 2;
     } else {
-        res.status(400);
         doubling = {
             "error": "Please provide an input!"
         }
     }
     res.send(doubling);
+})
+
+app.get('/greeter', (req, res) => {
+    let greeter = {};
+    console.log(req.query.name);
+    res.status(200);
+    res.setHeader("Content-type", "application/JSON");
+    if (req.query.name !== undefined && req.query.title !== undefined) {
+        greeter['welcome_message'] = "Oh, hi there " + req.query.name + ", my dear " + req.query.title + "!";
+    } else if (req.query.name == undefined && req.query.title !== undefined) {
+        greeter["error"] = "Please provide a name!"
+    } else if (req.query.name !== undefined && req.query.title == undefined) {
+        greeter["error"] = "Please provide a title!"
+    } else {
+        greeter["error"] = "Please provide a name and a title!"
+    }
+    res.send(greeter);
 })
 
 
