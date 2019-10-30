7818977cc209947bdd91139406343f23b1c25882
Krisztina Nagy <krisztinahont@hotmail.com>
2019-10-30 16:04:31 +0100

    added dountil

--- week-07/day-3/front-end/app.js
+++ week-07/day-3/front-end/app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 8080;
+const bodyParser = require('body-parser');
 app.use(express.static('assets'));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -54,6 +56,34 @@ app.get('/appenda/:appendable', (req, res) => {
     res.send(appenda);
 })
 
+app.post('/dountil/:action', (req, res) => {
+    let number = req.body.until;
+    let dountil = {};
+    console.log(req.body.until);
+    if (req.params.action === 'factor') {
+        dountil['result'] = factorio(number);
+    } else if (req.params.action === 'sum') {
+        dountil['result'] = sum(number);
+    } else {
+        dountil["error"] = "Please provide a number!";
+    }
+    res.send(dountil);
+})
+
+function factorio(num) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+function sum(num) {
+    let total = 0;
+    for (let i = 0; i <= num; i++) {
+        total += i;
+    }
+    return total;
+}
 
 app.listen(PORT, () => {
     console.log(`The server is up and running on ${PORT}`);
