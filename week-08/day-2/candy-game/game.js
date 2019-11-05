8d92bb68463a887107684519a642bbd287e58b75
Krisztina Nagy <krisztinahont@hotmail.com>
2019-11-05 15:19:24 +0100

    added candygame done

--- week-08/day-2/candy-game/game.js
+++ week-08/day-2/candy-game/game.js
@@ -14,6 +14,7 @@ let buyLollipops = document.querySelector(".buy-lollypops");
 let lollipops = document.querySelector(".lollypops");
 let candiesPerSecond = document.querySelector(".speed");
 let candyRain = document.querySelector(".candy-machine");
+candiesPerSecond.innerHTML = 0;
 candies.innerHTML = 900;
 lollipops.innerHTML = "🍭";
 
@@ -24,28 +25,24 @@ createCandy.addEventListener("click", function () {
 })
 
 buyLollipops.addEventListener("click", function () {
-    if (candies.innerHTML >= 100){
+    if (candies.innerHTML >= 100) {
         candies.innerHTML -= 100;
         lollipops.innerHTML += "🍭";
     }
 })
 
 let myInterval = setInterval(function () {
-    if (lollipops.innerHTML.length % 10 == 0 && lollipops.innerHTML.length !==0) {
+    if (lollipops.innerHTML.length % 10 == 0 && lollipops.innerHTML.length !== 0) {
         candiesPerSecond.innerHTML++;
         candies.innerHTML++;
-      }
+    }
+    candies.innerHTML = parseInt(candies.innerHTML) + parseInt(candiesPerSecond.innerHTML);
     if (candies.innerHTML > 10000) {
         console.log("I won");
         clearInterval(myInterval);
     }
 }, 1000);
 
-candyRain.addEventListener("click", function () {
-	candiesPerSecond.innerHTML = parseInt((candiesPerSecond.innerHTML) * 10)
-});
-
-setInterval(() => {
-	candies.innerHTML = parseInt(candies.innerHTML) + Math.floor(lollipops.innerHTML.length / 20) * candiesPerSecond.innerHTML;
-
-}, 1000);
+candyRain.onclick = () => {
+    candiesPerSecond.innerHTML = parseInt(candiesPerSecond.innerHTML) * 10
+};
