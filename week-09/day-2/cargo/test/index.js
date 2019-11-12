f22a2e5facea7943464157009e2311ff1b4774f8
Krisztina Nagy <krisztinahont@hotmail.com>
2019-11-12 15:30:20 +0100

    started test cases

--- week-09/day-2/cargo/test/index.js
+++ week-09/day-2/cargo/test/index.js
@@ -7,3 +7,16 @@ var app = require('../server');
 test('First test!', function (t) {
   t.end();
 });
+
+test('Correct pokemons returned', function (t) {
+    request(app)
+      .get('/api/pokemon')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        t.error(err, 'No error');
+        t.equal(res.body.length, 4, 'Correct number of pokemons');
+        t.equal(res.body[0].name, 'Pikachu', 'Correct name of first pokemon')
+        t.end();
+      });
+  });
