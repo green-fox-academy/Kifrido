0b00a8a4a918894fec345364d767953e6e2ae102
Krisztina Nagy <krisztinahont@hotmail.com>
2019-11-12 16:15:16 +0100

    added first testcase

--- week-09/day-2/cargo/test/index.js
+++ week-09/day-2/cargo/test/index.js
@@ -8,15 +8,14 @@ test('First test!', function (t) {
   t.end();
 });
 
-test('Correct pokemons returned', function (t) {
+test('overall status of the ship', function (t) {
     request(app)
-      .get('/api/pokemon')
+      .get('/rocket')
       .expect('Content-Type', /json/)
       .expect(200)
       .end(function (err, res) {
         t.error(err, 'No error');
-        t.equal(res.body.length, 4, 'Correct number of pokemons');
-        t.equal(res.body[0].name, 'Pikachu', 'Correct name of first pokemon')
+        t.deepEqual(res.body, { caliber25: 0, caliber30: 0, caliber50: 0, shipstatus: 'empty', ready: false }, 'overall status');
         t.end();
       });
   });
