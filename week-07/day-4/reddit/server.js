ca8f4f23114b136fff6406713a2a0c0f5e74b439
Krisztina Nagy <krisztinahont@hotmail.com>
2019-11-14 17:34:49 +0100

    changed folderstructure js not found

--- week-07/day-4/reddit/server.js
+++ week-07/day-4/reddit/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3000;
 const bodyParser = require('body-parser');
 app.use(express.static('assets'));
-app.use(bodyParser.json());
+app.use(bodyParser());
 app.use(express.json());
 const env = require('dotenv').config();
 const config = require('./config');
@@ -32,6 +32,15 @@ conn.query('SELECT * FROM reddit;', function (err, rows) {
 
 //creating get/posts
 
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/add', function (req, res) {
+    res.sendFile(__dirname + '/views/addpost.html');
+})
+
 app.get('/posts', function (req, res) {
     req.accepts('application/json');
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -50,6 +59,8 @@ app.get('/posts', function (req, res) {
 //creating post posts 
 
 app.post('/posts', (req, res) => {
+    console.log(req.body.url);
+    console.log(req.body.title);
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-type", "application/JSON");
     res.status(200);
@@ -100,4 +111,3 @@ app.put('/posts/:id/downvote', (req, res) => {
         res.send(rows);
     });
 })
-
--- week-07/day-4/reddit/views/addpost.js
+++ week-07/day-4/reddit/views/addpost.js
@@ -1,26 +1,26 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+/*const express = require('express');
+const app = express();*/
 /*const port = 3000;*/
-const bodyParser = require('body-parser');
+/*const bodyParser = require('body-parser');*/
 
-app.use(express.static('public'));
-app.use(express.json());
-app.use(bodyParser());
+/*app.use(express.static('public'));
+app.use(express.json());*/
+/*app.use(bodyParser());
+
+let ourRequest = new XMLHttpRequest();
+ourRequest.open('POST', 'http://localhost:3000/posts', true);
+
 
 let title = document.getElementById("title");
 console.log("title");
 
 title.addEventListener("input", function (event) {
-    if (title.validity.typeMismatch) {
-        title.setCustomValidity("Looser Uuuser");
-    } else {
-        title.setCustomValidity("");
-    }
+    console.log(req.body.title)
 });
 
-/*app.get('/', (req, res) => res.sendfile('index.html'));*/
+app.get('/', (req, res) => res.sendfile('index.html'));
 
 app.post('/posts', (req, res) => console.log(req.body.title));
 
@@ -36,5 +36,5 @@ url.addEventListener("input", function (event) {
 })
 
 app.post('/posts', (req, res) => console.log(req.body.url));
-
+*/
 /*app.listen(port, () => console.log(`Example app listening on port ${port}!`));*/
