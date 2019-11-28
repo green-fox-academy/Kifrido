c0ce065bdcf671057292d5739aaa475601c747a3
Krisztina Nagy <krisztinahont@hotmail.com>
2019-11-28 13:18:02 +0100

    post method working from front end

--- week-10/audio_project/assets/frontend.js
+++ week-10/audio_project/assets/frontend.js
@@ -1,9 +1,39 @@
 'use strict';
 
-function myFunction() {
-  let my_text = prompt('Enter text here');
-  if (my_text) alert(my_text);
-}
+let modalButton = document.querySelector(".modalButton");
+
+modalButton.addEventListener("click", () => {
+  fetch("http://localhost:3000/playlists", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ "playlist": playlist.value})
+  })
+    .then(response => response.json())
+   /* .then(ourData => {
+      p.innerText = `Your URL is aliased to ${ourData[0].alias} and your secret code is ${ourData[0].secretCode}.`
+      alias.value = "";
+      url.value = "";
+    })*/
+    .catch(error => {
+      console.log(error);
+      p.textContent = "Your alias is already in use!";
+      p.style.color = "red";
+    });
+  });
+  
+  
+/*function addPlaylistFunction() {
+  let addRequest = new XMLHttpRequest();
+  addRequest.open('POST', 'http://localhost:3000/playlists', true);
+
+  ourRequest.onload = function () {
+    let addData = JSON.parse(ourRequest.responseText);
+    console.log(addData);
+  }
+  ourRequest.send();
+}*/
+
+
 
 let source = document.querySelector('source');
 
@@ -43,3 +73,28 @@ myRequest.onload = function () {
 };
 
 myRequest.send();
+
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
